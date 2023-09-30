8132ca6e64861b23c2d92c5151e8eae104b5d2da
Tyler Petz <tylerpetz@gmail.com>
2023-09-30 17:16:54 -0600

    first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import Head from 'next/head';
-import * as React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+
 
 /**
  * SVGR Support
@@ -15,56 +11,65 @@ import UnstyledLink from '@/components/links/UnstyledLink';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Logo from '~/svg/Logo.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
+export interface Film {
+  title: string
+  year: string
+  thumbnail: string
+}
+
 export default function HomePage() {
+  const films: Film[] = useMemo(() => ([
+    {
+      title: 'Horsing Around',
+      year: '2021',
+      thumbnail: 'https://creepyparty.com/cdn/shop/products/p20-tuya_560x560.jpg',
+    },
+    {
+      title: 'Gangsters Paradise',
+      year: '2022',
+      thumbnail: 'https://media.gettyimages.com/id/125145388/photo/1940s-black-gangster.jpg?s=612x612&w=gi&k=20&c=uevS8wbmxrCxuY16TpKlSs8EoLJW5ooXGMQzW0fNZCA=',
+    },
+    {
+      title: 'Soulmatoes',
+      year: '2023',
+      thumbnail: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/tomatoes-1296x728-feature.jpg',
+    }
+  ]), [])
+
+  const [currentFilm, setCurrentFilm] = useState<Film>(films[0]);
+
+  const handleMouseOver = (film: Film) => {
+    setCurrentFilm(film);
+  }
+
+  // preload images
+  useEffect(() => {
+    films.forEach((film) => {
+      const img = new Image();
+      img.src = film.thumbnail;
+    });
+  }, [films]);
+
+  const selectedFilm = useMemo(() => {
+    const index = films.findIndex(film => film.title === currentFilm.title)
+    return films[index]
+  }, [films, currentFilm])
+
   return (
     <main>
-      <Head>
-        <title>Hi</title>
-      </Head>
-      <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
-
-          <ButtonLink className='mt-6' href='/components' variant='light'>
-            See all components
-          </ButtonLink>
-
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
-            />
-          </UnstyledLink>
-
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
-        </div>
+      <section className='relative flex min-h-screen flex-col items-start justify-end p-12 text-center space-y-8 bg-cover' style={{
+        backgroundImage: `url(${selectedFilm.thumbnail})`
+      }}>
+        {films.map((film) => (
+          <h1 className="text-white text-6xl relative" key={film.title} onMouseOver={() => handleMouseOver(film)} onFocus={() => handleMouseOver(film)}>
+            {film.title}<span className="text-sm relative -top-6 pl-4">{film.year}</span>
+          </h1>
+        ))}
       </section>
     </main>
   );
